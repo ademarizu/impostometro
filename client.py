1f9611753f2851afe230e4c1802577960d57f264
Ademar Izu Gomes <ademarizu@gmail.com>
2017-05-10 01:18:32 -0300

    Separating new calls for city and state

--- client.py
+++ client.py
@@ -2,12 +2,20 @@ import requests
 
 class ImpostometroClient:
 
-    def get_counter(self, state, start_date, end_date):
+    def get_state_counter(self, state, start_date, end_date):
         url = "https://impostometro.com.br/Contador/Estado?estado={0}s&dataInicial={1}&dataFinal={2}".format(state, start_date, end_date)
         headers = {'Accept-Encoding': 'gzip, deflate, sdch, br',
-                   # 'Accept-Language': 'pt-BR,pt;q=0.8,en-US;q=0.6,en;q=0.4',
-                   # 'Accept': '*/*',
-                   # 'Referer': 'https://impostometro.com.br/',
+                   'X-Requested-With': 'XMLHttpRequest'}
+        result = requests.get(url, headers=headers)
+        if result.status_code == 200:
+            counter = result.content
+        else:
+            couter = '{}'
+        return counter
+
+    def get_city_counter(self, state, city, start_date, end_date):
+        url = "https://impostometro.com.br/Contador/Municipios?estado={0}s&municipio={1}&dataInicial={2}&dataFinal={3}".format(state, city, start_date, end_date)
+        headers = {'Accept-Encoding': 'gzip, deflate, sdch, br',
                    'X-Requested-With': 'XMLHttpRequest'}
         result = requests.get(url, headers=headers)
         if result.status_code == 200:
@@ -24,9 +32,17 @@ class ImpostometroClientTestCase(unittest.TestCase):
     def setUpClass(cls):
         cls.client = ImpostometroClient()
 
-    def test_get_counter(self):
+    def test_get_state_counter(self):
         state = "sc"
         start_date = "01/01/2017"
         end_date = "09/05/2017"
-        result = self.client.get_counter(state, start_date, end_date)
+        result = self.client.get_state_counter(state, start_date, end_date)
+        print result
+
+    def test_get_state_counter(self):
+        state = "sc"
+        city = "florianopolis"
+        start_date = "01/01/2017"
+        end_date = "09/05/2017"
+        result = self.client.get_city_counter(state, city, start_date, end_date)
         print result
--- server.py
+++ server.py
@@ -10,21 +10,22 @@ ic = ImpostometroClient()
 @app.route('/counter')
 def counter():
     state = request.args.get('state')
+    city = request.args.get('city')
     start_date = request.args.get('start_date')
     end_date = request.args.get('end_date')
     callback = request.args.get('callback')
 
-    counter = handle_counter(state, start_date, end_date)
+    if city:
+        counter = ic.get_city_counter(state, city, start_date, end_date)
+    else:
+        counter = ic.get_state_counter(state, start_date, end_date)
+
     if callback:
         result = "{0}({1});".format(callback, counter)
     else:
         result = counter
 
     return Response(result, mimetype='application/javascript')
-
-
-def handle_counter(state, start_date, end_date):
-    return ic.get_counter(state, start_date, end_date)
 
 
 if __name__ == '__main__':
